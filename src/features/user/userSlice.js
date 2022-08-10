fac98ac766bda988164574f7236a61292454250c
KIRILL F <tiltlessman@gmail.com>
2022-08-10 14:18:58 +0300

    register page and logic

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,62 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify'
+import customFetch from '../../utils/axios'
+
+const initialState = {
+  isLoading: false,
+  user: null,
+}
+
+export const registerUser = createAsyncThunk(
+  'user/registerUser',
+  async (user, thunkAPI) => {
+    try {
+      const resp = await customFetch.post('/auth/testingRegister')
+      return resp.data
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg)
+    }
+  }
+)
+
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async (user, thunkAPI) => {
+    console.log(`Login User: ${JSON.stringify(user)}`)
+  }
+)
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  extraReducers: {
+    [registerUser.pending]: (state) => {
+      state.isLoading = true
+    },
+    [registerUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload
+      state.isLoading = false
+      state.user = user
+      toast.success(`Hello There ${user.name})`)
+    },
+    [registerUser.rejected]: (state, { payload }) => {
+      state.isLoading = false
+      toast.error(payload)
+    },
+    [loginUser.pending]: (state) => {
+      state.isLoading = true
+    },
+    [loginUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload
+      state.isLoading = false
+      state.user = user
+      toast.success(`Welcome back ${user.name})`)
+    },
+    [loginUser.rejected]: (state, { payload }) => {
+      state.isLoading = false
+      toast.error(payload)
+    },
+  },
+})
+
+export default userSlice.reducer
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { Logo } from '../components'
+import { Logo, FormRow } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
+import { toast } from 'react-toastify'
+import { useSelector, useDispatch } from 'react-redux'
+import { loginUser, registerUser } from '../features/user/userSlice'
 
 const initialState = {
   name: '',
@@ -12,36 +15,74 @@ const initialState = {
 
 function Register() {
   const [values, setValues] = useState(initialState)
+  const { user, isLoading } = useSelector((store) => store.user)
+  const dispatch = useDispatch()
 
   const handleChange = (e) => {
-    console.log(e.targer)
+    const name = e.target.name
+    const value = e.target.value
+    setValues({ ...values, [name]: value })
   }
   const onSubmit = (e) => {
     e.preventDefault()
-    console.log(e.target)
+    const { name, email, password, isMember } = values
+    if (!email || !password || (!isMember && !name)) {
+      toast.warning('Please fill out all fields')
+      return
+    }
+    if (isMember) {
+      dispatch(loginUser({ email: email, password: password }))
+      return
+    }
+    dispatch(registerUser({ name, email, password }))
+  }
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember })
   }
 
   return (
     <Wrapper className='full-page'>
       <form className='form' onSubmit={onSubmit}>
         <Logo />
-        <h3>Login</h3>
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
         {/* name field */}
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input
+        {!values.isMember && (
+          <FormRow
             type='text'
             name='name'
             value={values.name}
-            onChange={handleChange}
-            className='form-input'
+            handleChange={handleChange}
           />
-          <button type='submit' className='btn btn-block'>
-            submit
+        )}
+
+        {/* email field */}
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password field */}
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type='submit' className='btn btn-block'>
+          {isLoading ? 'loading...' : 'submit'}
+        </button>
+        <p>
+          {values.isMember ? 'Not a member yet?' : 'Already a member'}
+          <button
+            type='button'
+            onClick={toggleMember}
+            className='member-btn'
+            disabled={isLoading}
+          >
+            {values.isMember ? 'Register' : 'Login'}
           </button>
-        </div>
+        </p>
       </form>
     </Wrapper>
   )
